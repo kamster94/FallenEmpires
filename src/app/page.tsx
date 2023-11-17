b7033f6e59ab344240a5c21dc8dbea0afd5de61c
kamster94 <kamster94@gmail.com>
2023-11-17 18:13:15 +0100

    Fixed home page text to be dynamic, so vercel does not cache it as static

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,28 @@
+'use client';
+
 import Page from '@/components/Page';
 import PageHeader from '@/components/PageHeader';
 import { getGeneralSetting } from '@/app/actions';
 import MarkdownContent from '@/components/Markdown/MarkdownContent';
 import { GeneralSettingKey } from '@/enums';
+import { useEffect, useState } from 'react';
 
-export default async function Home() {
-  const homePageText = await getGeneralSetting(GeneralSettingKey.HomePageText);
+export default function Home() {
+  const [homePageText, setHomePageText] = useState<string>('');
+  useEffect(() => {
+    getGeneralSetting(GeneralSettingKey.HomePageText).then(
+      (homePageTextSetting) => {
+        if (homePageTextSetting) {
+          setHomePageText(homePageTextSetting.value ?? '');
+        }
+      }
+    );
+  }, []);
 
   return (
     <Page>
       <PageHeader title='Age of Fallen Empires' />
-      <MarkdownContent>{homePageText?.value ?? ''}</MarkdownContent>
+      <MarkdownContent>{homePageText}</MarkdownContent>
     </Page>
   );
 }
